147c56ac9d3fa2523f9f8f6cec5f08a3ec1366f1
Guilherme Gehring <guilherme.gehring@gmail.com>
2022-04-13 17:52:10 -0300

    Iniciando princípio ISP

---
+++ ISP/app_crm/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6df87f2b1a37fc9acc49ea6640a678236d0163aa',
+    'name' => 'guilherme/app_crm',
+  ),
+  'versions' => 
+  array (
+    'guilherme/app_crm' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6df87f2b1a37fc9acc49ea6640a678236d0163aa',
+    ),
+  ),
+);
